253ae7ebe0ae33c068ab538716205cb9b02b7d79
Alexey Beltyukov <lesha.beltyukov@gmail.com>
2023-04-25 18:33:20 +0400

    Все начальные настройки применены, созданы папки и файлы. Также стили к навигационному меню.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import Homepage from './Pages/Homepage';
 import './App.css';
+import About from './Pages/About';
+import Contacts from './Pages/Contacts';
+import Service from './Pages/Service';
+import logo from './img/logo.png'
+import { Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='zagolovok'>
+    <header>
+      <img src={logo} alt='' className='logo'></img>
+      <ul className='nav'>
+        <li><Link to='/'>Главная</Link></li>
+        <li><Link to='/contacts'>Контакты</Link></li>
+        <li><Link to='/service'>Услуги</Link></li>
+        <li><Link to='/about'>О нас</Link></li>
+      </ul>
+    </header>
+    <Routes>
+      <Route path='/' element={<Homepage/>}></Route>
+      <Route path='/contacts' element={<Contacts/>}></Route>
+      <Route path='/service' element={<Service/>}></Route>
+      <Route path='/about' element={<About/>}></Route>
+    </Routes>
     </div>
+    
   );
 }
 
---
+++ src/Pages/Contacts.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class Contacts extends Component {
+  render() {
+    return (
+      <div className='contacts'>контакты</div>
+    )
+  }
+}
+
+export default Contacts
